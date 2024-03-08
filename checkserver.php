02528ef0f73009b3ef4dc3afc12f9f33694e109c
borges <78981485+xyborges@users.noreply.github.com>
2024-03-08 17:57:22 -0300

    Update checkserver.php

--- checkserver.php
+++ checkserver.php
@@ -1,11 +1,1 @@
-<?php
 
-$ip = $_SERVER['REMOTE_ADDR'];
-$arquivo_registro = 'rrg.txt';
-
-$handle = fopen($arquivo_registro, 'a');
-fwrite($handle, $ip . "\n");
-fclose($handle);
-
-echo "IP registrado com sucesso!";
-?>
