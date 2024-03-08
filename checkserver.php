4ed0d0e52897a9f8931c511438294b0b0d1946d6
borges <78981485+xyborges@users.noreply.github.com>
2024-03-08 17:53:41 -0300

    Update checkserver.php

--- checkserver.php
+++ checkserver.php
@@ -1,7 +1,11 @@
 <?php
-	$ip = $_SERVER['REMOTE_ADDR'];
-	$arquivo_registro = 'registro_visitas.txt';
-	$handle = fopen($arquivo_registro, 'a');
-	fwrite($handle, $ip . "\n");
-	fclose($handle);
+
+$ip = $_SERVER['REMOTE_ADDR'];
+$arquivo_registro = 'registro_visitas.txt';
+
+$handle = fopen($arquivo_registro, 'a');
+fwrite($handle, $ip . "\n");
+fclose($handle);
+
+echo "IP registrado com sucesso!";
 ?>
